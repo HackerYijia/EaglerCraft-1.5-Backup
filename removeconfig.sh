69c8a403c430ad6eaf7a3217a5520560cb5f729d
诺影科技 NoxShadow Tech <115096818+HackerYijia@users.noreply.github.com>
2023-04-04 04:35:40 +0800

    Create removeconfig.sh

---
+++ removeconfig.sh
@@ -0,0 +1,5 @@
+rm java/bungee_command/config
+rm java/bukkit_command/config
+
+echo "Done removing all of the useless config thing that GitHubs need to make an folder,"
+echo "They are no longer needed in Replit/Codeanywhere."
